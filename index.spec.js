e5bce6dbebc274d27f1358af90a3fd15593f155c
Mihai <mihai.babet@gmail.com>
2019-01-30 08:25:04 +0200

    add test

--- index.spec.js
+++ index.spec.js
@@ -18,6 +18,31 @@ describe('test universal reducers', () => {
     })
   })
 
+  it('should update a nested value', () => {
+    const state = {
+      parent: {
+        child1: {
+          child2: [{ value: '123' }]
+        }
+      }
+    }
+    const newState = reducer(state, {
+      type: UNIVERSAL_ACTION_TYPE,
+      path: 'parent.child1.child2.1.value',
+      payload: '126'
+    })
+    assert.deepEqual(newState, {
+      parent: {
+        child1: {
+          child2: [
+            { value: '123' },
+            { value: '126' }
+          ]
+        }
+      }
+    })
+  })
+
   it('should delete delete a nested value', () => {
     const state = {
       parent: {
