dd4ce8740a7d415f1fff39d35ae1d17ca6335462
Mihai Babet <mihai.babet@gmail.com>
2019-11-21 19:54:46 +0200

    update reducer

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
-import set from 'lodash/fp/set'
+import setWith from 'lodash/fp/setWith'
 import unset from 'lodash/fp/unset'
-import update from 'lodash/fp/update'
+import updateWith from 'lodash/fp/updateWith'
 import isPlainObject from 'lodash/isPlainObject'
 import get from 'lodash/get'
+import isNil from 'lodash/isNil'
+import isFunction from 'lodash/isFunction'
 
 export const createReducer = (actionTypes) => {
   const reducer = (state = {}, action = {}) => {
-    const { type, path = '', payload } = action
-    if (type === actionTypes.set) return set(path, payload, state)
-    if (type === actionTypes.delete) return unset(path, state)
-    if (type === actionTypes.update) return update(path, (value) => {
-      if (isPlainObject(value) && isPlainObject(payload)) {
-        return { ...value, ...payload }
-      } else {
-        return payload
+    const { type, path, payload } = action
+    if (!path) {
+      switch (type) {
+        case (actionTypes.set): {
+          if (isNil(payload)) return ({})
+          return payload
+        }
+        case (actionTypes.update):
+          switch (true) {
+            case (isNil(payload)): return state
+            case (isFunction(payload)): return payload(state)
+            case (isPlainObject(state) && isPlainObject(payload)):
+              return { ...state, ...payload }
+            default: return payload
+          }
+        case (actionTypes.delete): return ({})
+        default: return state
       }
-    }, state)
-    return state
+    } else {
+      switch (type) {
+        case (actionTypes.set): {
+          if (isNil(payload)) return unset(path, state)
+          return setWith(Object, path, payload, state)
+        }
+        case (actionTypes.update):
+          switch (true) {
+            case (isNil(payload)): return state
+            case (isFunction(payload)):
+              return updateWith(Object, path, payload, state)
+            default: return updateWith(Object, path, (value) => {
+              if (isPlainObject(value) && isPlainObject(payload)) {
+                return { ...value, ...payload }
+              } else {
+                return payload
+              }
+            }, state)
+          }
+        case (actionTypes.delete): return unset(path, state)
+        default: return state
+      }
+    }
   }
   return reducer
 }
@@ -24,45 +56,7 @@ export const createReducer = (actionTypes) => {
 export const createStoreApi = ({ store, actionTypes }) => ({
   select: (selector) => selector(store.getState()),
   get: (path, defautValue) => get(store.getState(), path, defautValue),
-  set: (path, payload) => new Promise((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        store.dispatch({
-          path,
-          payload,
-          type: actionTypes.set
-        })
-        return resolve(payload)
-      } catch (error) {
-        return reject(error)
-      }
-    }, 0)
-  }),
-  delete: (path) => new Promise((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        store.dispatch({
-          path,
-          type: actionTypes.delete
-        })
-        return resolve()
-      } catch (error) {
-        return reject(error)
-      }
-    }, 0)
-  }),
-  update: (path, payload) => new Promise((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        store.dispatch({
-          path,
-          payload,
-          type: actionTypes.update
-        })
-        return resolve(payload)
-      } catch (error) {
-        return reject(error)
-      }
-    }, 0)
-  })
+  set: (path, payload) => store.dispatch({ path, payload, type: actionTypes.set }),
+  delete: (path) => store.dispatch({ path, type: actionTypes.delete }),
+  update: (path, payload) => store.dispatch({ path, payload, type: actionTypes.update })
 })
