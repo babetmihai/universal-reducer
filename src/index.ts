4aa130baac768a4c441e3b50509c2f136c973d03
Mihai Babet <mihai.babet@gmail.com>
2022-01-29 20:39:01 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,20 @@
 import setWith from "lodash/fp/setWith";
 import unset from "lodash/fp/unset";
 import updateWith from "lodash/fp/updateWith";
-import { createStore } from "redux";
 import get from 'lodash/get'
 
 
 const EMPTY_OBJECT = {};
 
-const SET = "@SET";
-const UPDATE = "@UPDATE";
-const UNSET = "@UNSET";
-const INIT = "@INIT"
+const SET = "@@SET";
+const UPDATE = "@@UPDATE";
+const UNSET = "@@UNSET";
 
 
 const reducer = (state: any = EMPTY_OBJECT, action: any = EMPTY_OBJECT) => {
   const { type, method, payload } = action;
 
   switch (method) {
-    case INIT: return payload
     case SET:  return setWith(Object, type, payload, state);
     case UPDATE: return updateWith(Object, type, payload, state);
     case UNSET: return unset(type, state);
@@ -31,21 +28,13 @@ const reducer = (state: any = EMPTY_OBJECT, action: any = EMPTY_OBJECT) => {
 };
 
 
-export const createUniversalStore = (initialState, ...middleware) => {
-  const store:any = createStore(
-    reducer,
-    initialState,
-    ...middleware
-  )
+export const createActions = (store) => ({
+  get: (path: string, defautValue?: any): any => get(store.getState(), path, defautValue),
+  set: (path: string, payload: any) => store.dispatch({ type: path, payload, method: SET }),
+  unset: (path: string) => store.dispatch({ type: path, method: UNSET }),
+  update: (path: string, payload: Function) => store.dispatch({ type: path, payload, method: UPDATE }),
+})
 
-  store.get = (path: string, defautValue?: any): any => get(store.getState(), path, defautValue)
-  store.set = (path: string, payload: any) => store.dispatch({ type: path, payload, method: SET })
-  store.unset = (path: string) => store.dispatch({ type: path, method: UNSET })
-  store.update = (path: string, payload: Function) => store.dispatch({ type: path, payload, method: UPDATE })
-
-  return store
-}
 
 
 export default reducer
-
