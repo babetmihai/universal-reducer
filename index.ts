6c68b287ee79c3f08d5e2d9222bb07804f3d5bc0
Mihai Babet <mihai.babet@gmail.com>
2022-02-20 16:11:31 +0200

    fix reducer

--- index.ts
+++ index.ts
@@ -7,9 +7,14 @@ import setWith from "lodash/fp/setWith";
 import unset from "lodash/fp/unset";
 import updateWith from "lodash/fp/updateWith";
 import get from 'lodash/get'
+import isNil from 'lodash/isNil'
+import isFunction from 'lodash/isFunction'
+import isPlainObject from 'lodash/isPlainObject'
 export * from 'redux'
 export * from 'react-redux'
 
+
+const EMPTY_OBJECT = {}
 
 const SET = "@@SET";
 const UPDATE = "@@UPDATE";
@@ -19,6 +24,7 @@ const UNSET = "@@UNSET";
 type IAction = {
   type: any;
   method: string;
+  path: IPath;
   payload: any;
 }
 
@@ -26,14 +32,38 @@ type IPath = Array<string | number> | string | number
 
 
 export const reducer = (
-  state: any = {},
+  state: any = EMPTY_OBJECT,
   action: IAction
 ) => {
-  const { type: path, method, payload } = action
+  const { path, method, payload } = action
   switch (method) {
-    case SET: return setWith(Object, path, payload, state);
-    case UPDATE: return updateWith(Object, path, payload, state);
-    case UNSET: return unset(path, state);
+    case SET: {
+      if (isNil(path)) return payload
+      return setWith(Object, path, payload, state)
+    }
+    case UPDATE: {
+      if (isNil(payload)) return state
+      if (isNil(path)) {
+        if (isFunction(payload)) return payload(state)
+        if (isPlainObject(state) && isPlainObject(payload)) {
+          return { ...payload, ...payload }
+        }
+        return payload
+      } else {
+        if (isFunction(payload)) return updateWith(Object, path, payload, state)
+        return updateWith(Object, path, (value) => {
+          if (isPlainObject(value) && isPlainObject(payload)) {
+            return { ...value, ...payload }
+          } else {
+            return payload
+          }
+        }, state)
+      }
+    }
+    case UNSET: {
+      if (isNil(path)) return EMPTY_OBJECT
+      return unset(path, state);
+    }
     default: return state;
   }
 };
@@ -44,37 +74,123 @@ export const createActions = (store) => {
     /**
      *  Gets the value at path of the state object. If the resolved value is undefined, the defaultValue is returned in its place. https://lodash.com/docs/4.17.21#get
     */
-    get: (path: IPath, defautValue?: any): any => get(store.getState(), path, defautValue),
+    get: (...args: [path: IPath, defautValue?: any] | []): any => {
+      if (args.length === 0) return store.getState()
+      if (args.length > 0) {
+        const [path, defaultValue] = args
+        return get(store.getState(), path, defaultValue)
+      }
+    },
     /**
      *  Sets the value at path of the state object. If a portion of path doesn't exist, it's created. https://lodash.com/docs/4.17.21#set
     */
-    set: (path: IPath, payload: any): void => store.dispatch({
-      type: path,
-      payload,
-      method: SET
-    }),
+    set: (...args: [path: IPath, payload: any] | [payload: any]): void => {
+      if (args.length === 1) {
+        const [payload] = args
+        store.dispatch({
+          type: 'Set',
+          payload,
+          method: SET
+        })
+      }
+      if (args.length > 1) {
+        const [path, payload] = args
+        store.dispatch({
+          type: `Set: ${path}`,
+          path,
+          payload,
+          method: SET
+        })
+      }
+    },
     /**
      *  This method is like set except that accepts updater to produce the value to set. https://lodash.com/docs/4.17.21#update
     */
-    update: (path: IPath, payload: Function): void => store.dispatch({
-      type: path,
-      payload,
-      method: UPDATE
-    }),
+    update: (...args: [path: IPath, payload: Function] | [payload: Function]): void => {
+      if (args.length === 1) {
+        const [payload] = args
+        store.dispatch({
+          type: 'Update',
+          payload,
+          method: UPDATE
+        })
+      }
+      if (args.length > 1) {
+        const [path, payload] = args
+        store.dispatch({
+          type: `Update: ${path}`,
+          path,
+          payload,
+          method: UPDATE
+        })
+      }
+    },
     /**
      *  Removes the property at path of state object. https://lodash.com/docs/4.17.21#unset
     */
-    unset: (path: IPath): void => store.dispatch({
-      type: path,
-      method: UNSET
-    }),
+    unset: (...args: [path: IPath] | []): void => {
+      if (args.length === 0) {
+        store.dispatch({
+          type: `Unset`,
+          method: UNSET
+        })
+      }
+      if (args.length > 0) {
+        const [path] = args
+        store.dispatch({
+          type: `Unset: ${path}`,
+          path,
+          method: UNSET
+        })
+      }
+    },
     /**
      *  Creates a new actions module localized at the path of the state object. 
      */
-    create: (path: IPath) => {
+    create: (modulePath: IPath) => {
       return Object.keys(actions)
-        .reduce((acc, key) => {
-          acc[key] = (_path, ...args) => actions[key](`${path}.${_path}`, ...args)
+        .reduce((acc, actionKey) => {
+          acc[actionKey] = (...args) => {
+            switch (actionKey) {
+              case ('get'): {
+                if (args.length === 0) return actions[actionKey](modulePath)
+                if (args.length > 0) {
+                  const [path, defaultValue] = args
+                  return actions[actionKey](`${modulePath}.${path}`, defaultValue)
+                }
+                break
+              }
+              case ('update'):
+              case ('set'): {
+                if (args.length === 1) {
+                  const [payload] = args
+                  return actions[actionKey](modulePath, payload)
+                }
+                if (args.length > 1) {
+                  const [path, payload] = args
+                  return actions[actionKey](`${modulePath}.${path}`, payload)
+                }
+                break
+              }
+              case ('unset'): {
+                if (args.length === 0) {
+                  return actions[actionKey](modulePath)
+                }
+                if (args.length > 0) {
+                  const [path] = args
+                  return actions[actionKey](`${modulePath}.${path}`)
+                }
+                break
+              }
+              case ('create'): {
+                const [path] = args
+                return actions[actionKey](`${modulePath}.${path}`)
+              }
+              default: {
+                // do nothing
+              }
+            }
+          }
           return acc
         }, {})
     }
